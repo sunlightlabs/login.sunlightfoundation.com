b5ab89e8b31b95385b153bef72b8a5161438b9bc
Paul Tagliamonte <paultag@debian.org>
2012-11-20 14:49:50 -0500

    update to html

--- shoelace/apps/accounts/urls.py
+++ shoelace/apps/accounts/urls.py
@@ -8,6 +8,13 @@ urlpatterns = patterns('',
             'template_name': 'accounts/login.html'
         },
     ),
+    url(
+        r'^logout/$',
+        'django.contrib.auth.views.logout',
+        {
+            'template_name': 'accounts/logout.html'
+        },
+    ),
     url(r'^profile/$', 'shoelace.apps.accounts.views.profile')
 
 )
