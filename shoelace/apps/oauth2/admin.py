05b264b8243d180c7d7b534b003632c301439a24
Paul Tagliamonte <paultag@debian.org>
2012-11-20 16:22:25 -0500

    adding in some other things

--- shoelace/apps/oauth2/admin.py
+++ shoelace/apps/oauth2/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from oauth2app.models import Client
+from oauth2app.models import Client, AccessRange
 
 
 class ClientAdmin(admin.ModelAdmin):
     exclude = ('redirect_uri',)
 
 admin.site.register(Client, ClientAdmin)
+
+
+class AccessRangeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(AccessRange, AccessRangeAdmin)
