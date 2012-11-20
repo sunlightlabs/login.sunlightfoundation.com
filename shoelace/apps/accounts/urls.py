8deb9d5b6b07cac7969bad48b09a5e0b4007799c
Paul Tagliamonte <paultag@debian.org>
2012-11-20 14:28:45 -0500

    homepage things

--- shoelace/apps/accounts/urls.py
+++ shoelace/apps/accounts/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    (
+    url(
         r'^login/$',
         'django.contrib.auth.views.login',
         {
             'template_name': 'accounts/login.html'
-        }
+        },
     ),
-    (r'^profile/$', 'shoelace.apps.accounts.views.profile'),
+    url(r'^profile/$', 'shoelace.apps.accounts.views.profile')
 
 )
