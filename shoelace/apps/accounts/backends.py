38c9b330a2fe0b13b6de24eef59c99d59e5fb899
James Turk <james.p.turk@gmail.com>
2012-11-29 18:48:41 -0500

    fix EmailBackend

--- shoelace/apps/accounts/backends.py
+++ shoelace/apps/accounts/backends.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
 
 
-class EmailBackend(object):
+class EmailBackend(ModelBackend):
     """ very simple class to authenticate w/ email instead of username """
 
     def authenticate(self, username, password):
         try:
-            user = User.objects.get({'email': username})
+            user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
