0bd8ff9662f76dc6d5ef2b396ecac00409ceb2b1
James Turk <james.p.turk@gmail.com>
2012-11-29 19:13:30 -0500

    add urls for password reset/change views, templates needed

--- shoelace/apps/accounts/urls.py
+++ shoelace/apps/accounts/urls.py
@@ -1,20 +1,60 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
+    # contrib.auth views
     url(
         r'^login/$',
         'django.contrib.auth.views.login',
-        {
-            'template_name': 'accounts/login.html'
-        },
+        { 'template_name': 'accounts/login.html' },
+        name='login',
     ),
     url(
         r'^logout/$',
         'django.contrib.auth.views.logout',
-        {
-            'template_name': 'accounts/logout.html'
-        },
+        { 'template_name': 'accounts/logout.html' },
+        name='logout',
     ),
+    url(
+        r'^password_change/$',
+        'django.contrib.auth.views.password_change',
+        {'template_name': 'accounts/password_change_form.html',
+        },
+        name='password_change',
+    ),
+    url(
+        r'^password_change_done/$',
+        'django.contrib.auth.views.password_change_done',
+        { 'template_name': 'accounts/password_change_done.html' },
+        name='password_change_done',
+    ),
+    url(
+        r'^password_reset/$',
+        'django.contrib.auth.views.password_reset',
+        {'template_name': 'accounts/password_reset_form.html',
+         'email_template_name': 'accounts/password_reset_email.html',
+         'subject_template_name': 'accounts/password_reset_subject.txt',
+        },
+        name='password_reset',
+    ),
+    url(
+        r'^password_reset_done/$',
+        'django.contrib.auth.views.password_reset_done',
+        { 'template_name': 'accounts/password_reset_done.html' },
+        name='password_reset_done',
+    ),
+    url(
+        r'^password_reset_confirm/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        { 'template_name': 'accounts/password_reset_confirm.html' },
+        name='password_reset_confirm',
+    ),
+    url(
+        r'^password_change_done/$',
+        'django.contrib.auth.views.password_reset_complete',
+        { 'template_name': 'accounts/password_reset_complete.html' },
+        name='password_reset_complete',
+    ),
+    # custom views
     url(
         r'^profile/$',
         'shoelace.apps.accounts.views.profile',
