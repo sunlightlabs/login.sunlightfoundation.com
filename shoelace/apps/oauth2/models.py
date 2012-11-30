58e81a8def4bff582ebde161738af944dac887e6
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-11-30 10:50:39 -0500

    Adding in ClientProfile stuff for themeing.

---
+++ shoelace/apps/oauth2/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from oauth2app.models import Client
+
+
+class ClientProfile(models.Model):
+    client = models.OneToOneField(Client)
+
+    template_slug = models.CharField(max_length=30)
+    image_url = models.CharField(max_length=256)
