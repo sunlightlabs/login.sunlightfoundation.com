905d33c0599302c1fa2c99ab7bd640c9df289b21
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-12-03 10:35:50 -0500

    adding proper validation - Close: #4

--- shoelace/apps/accounts/forms.py
+++ shoelace/apps/accounts/forms.py
@@ -12,9 +12,21 @@ class SignupForm(forms.Form):
     password2 = forms.CharField(label='Confirm Password',
                                 widget=PasswordInput())
 
-    def is_valid_email(self, field_data, all_data):
+    def clean_email(self):
         try:
-            User.objects.get(email=field_data)
+            User.objects.get(email=self.data['email'])
         except User.DoesNotExist:
             return
-        raise validators.ValidationError('The email "%s" is already taken.' % field_data)
+        raise validators.ValidationError(
+            'The email "%s" is already taken.' % (self.data['email'])
+        )
+
+    def clean_password(self):
+        if self.data['password'] != self.data['password2']:
+            raise forms.ValidationError('Passwords are not the same')
+        return self.data['password']
+
+    def clean(self,*args, **kwargs):
+        self.clean_email()
+        self.clean_password()
+        return super(SignupForm, self).clean(*args, **kwargs)
